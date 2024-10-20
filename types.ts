bc7e04195ec0a886b841617044005b6f4f6b4b58
RicardoArquimedes <richardcqt28@gmail.com>
2024-10-20 18:28:36 -0500

    :sparkles: adding file types.ts to describe types in TS

---
+++ types.ts
@@ -0,0 +1,17 @@
+let age: number = 34;
+let firstName: string = "Ricardo";
+let isSenior: boolean = true;
+let person: Object = {};
+let fruits: string[] = ["apple", "banana", "strawberry"];
+
+let data: Object[] = [{}, {name: firstName}]
+let response: any = "Hola"
+response = 20;
+
+function greeting (): void {
+    console.log("Hola", firstName)
+}
+
+let response2: unknown;
+response2 = true;
+
